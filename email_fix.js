1843f45d876371b06319dcac565f09b9494654f4
Tyler <alexithemia@gmail.com>
2019-01-10 10:38:23 -1000

    Added webpage setup to run any email log

--- email_fix.js
+++ email_fix.js
@@ -1,18 +1,49 @@
-let dataset = require('./email_log.json');
-function emailFix(dataset) {
-  let addresses = dataset.emails.reduce(function (emails, currentAddress) {
-    // console.log(emails)
-    // console.log(currentAddress);
+// let dataset = require('./email_log.json');
+function emailFix() {
+  fetch(savedLink).then(function (response) {
+    // console.log(response.json());
+    return (response.json());
+  })
+    .then(function (myJson) {
+      // let dataset = require('./email_log.json');
+      let addresses = myJson.emails.reduce(function (emails, currentAddress) {
+        // console.log(emails)
+        // console.log(currentAddress);
 
-    if (!emails.hasOwnProperty(currentAddress.email)) {
-      emails[currentAddress.email] = null;
-    }
-    emails[currentAddress.email]++;
+        if (!emails.hasOwnProperty(currentAddress.email)) {
+          emails[currentAddress.email] = null;
+        }
+        emails[currentAddress.email]++;
 
-    return emails;
-  }, {});
+        return emails;
+      }, {});
+      let results = document.getElementById('results');
+      results.innerHTML = '';
+      for (key in addresses) {
+        if (addresses.hasOwnProperty(key)) {
+          console.log(key)
+          console.log(addresses[key]);
 
-  console.log(addresses);
+          results.innerHTML += key + ':' + addresses[key] + '<br>';
+        }
+      }
+      console.log(addresses);
+    });
+
 }
-emailFix(dataset);
+// emailFix(dataset);
 
+let input = document.getElementById('link');
+input.addEventListener('input', function (event) {
+  saveLink(event.target.value);
+});
+let savedLink = '';
+function saveLink(link) {
+  savedLink = link;
+  console.log(savedLink)
+}
+
+let checkBtn = document.getElementById('check');
+checkBtn.addEventListener('click', function () {
+  emailFix();
+});
