1f503fb5d90722d3b3a1cb935daf1838a23c4018
Tyler <alexithemia@gmail.com>
2019-01-10 09:37:31 -1000

    Module setup

--- email_fix.js
+++ email_fix.js
@@ -1,17 +1,18 @@
 let dataset = require('./email_log.json');
-let addresses = dataset.emails.reduce(function (emails, currentAddress) {
-  // console.log(emails)
-  // console.log(currentAddress);
+function emailFix(dataset) {
+  let addresses = dataset.emails.reduce(function (emails, currentAddress) {
+    // console.log(emails)
+    // console.log(currentAddress);
 
-  if (!emails.hasOwnProperty(currentAddress.email)) {
-    emails[currentAddress.email] = null;
-  }
-  emails[currentAddress.email]++;
+    if (!emails.hasOwnProperty(currentAddress.email)) {
+      emails[currentAddress.email] = null;
+    }
+    emails[currentAddress.email]++;
 
-  return emails;
-}, {});
+    return emails;
+  }, {});
 
-console.log(addresses);
-
-
+  console.log(addresses);
+}
+emailFix(dataset);
 
